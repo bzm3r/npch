51b70bb9bb5b4400fc216f521935b1ecee1b1065
Brian Merchant <bhmerchant@gmail.com>
2023-01-25 12:59:29 -0800

    Make sure Specials text doesn't overflow past central block

--- src/routes/Specials.svelte
+++ src/routes/Specials.svelte
@@ -1,9 +1,17 @@
 <script>
-    export let specials;
+	export let specials;
 </script>
 
-<ul>
-	{#each specials as special}
-		<li>{special}</li>
-	{/each}
-</ul>
+<div class="container">
+	<ul>
+		{#each specials as special}
+			<li>{special}</li>
+		{/each}
+	</ul>
+</div>
+
+<style>
+	.container {
+		width: 300px;
+	}
+</style>
